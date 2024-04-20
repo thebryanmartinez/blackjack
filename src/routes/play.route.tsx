1dc8b16fec46842dcff27cfbd67a91d8f0966e11
thebryanmartinez <bryanmartz972@gmail.com>
2024-04-19 22:27:56 -0600

    feat: 🎸 finish first playable version for the blackjack app

--- src/routes/play.route.tsx
+++ src/routes/play.route.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { getDeckOfCards, getCard } from '@/services'
 import { useState } from 'react'
 import { Card } from '@/models'
+import { cardDictionary } from '@/utils/cardsDictionaryImages'
 
 const Play = () => {
   const [deckId, setDeckId] = useState<string | null>(null)
@@ -10,26 +11,51 @@ const Play = () => {
   const [dealerCards, setDealerCards] = useState<Card[]>([])
   const [playerScore, setPlayerScore] = useState<number>(0)
   const [dealerScore, setDealerScore] = useState<number>(0)
+  const [gameWinnerText, setGameWinnerText] = useState<string>('')
+  const [gameRestart, setGameRestart] = useState<boolean>(false)
+  const [hitLoading, setHitLoading] = useState<boolean>(false)
+  const [stayLoading, setStayLoading] = useState<boolean>(false)
+  const [dealersTurn, setDealerTurn] = useState<boolean>(false)
 
   useEffect(() => {
     const getDeck = async () => {
       const deck = await getDeckOfCards()
 
       if (deck) {
-        const playerCards = await getCard(deck, 2)
-        const dealerCards = await getCard(deck, 2)
+        Promise.all([getCard(deck, 2), getCard(deck, 2)]).then(
+          ([playerCards, dealerCards]) => {
+            setPlayerCards(playerCards)
+            setDealerCards(dealerCards)
+            setPlayerScore(checkCardsValue(playerCards))
+            setDealerScore(checkCardsValue(dealerCards))
+          }
+        )
 
         setDeckId(deck)
-        setPlayerCards(playerCards)
-        setDealerCards(dealerCards)
-
-        setPlayerScore(checkCardsValue(playerCards))
-        setDealerScore(checkCardsValue(dealerCards))
       }
     }
 
     getDeck()
-  }, [])
+  }, [gameRestart])
+
+  useEffect(() => {
+    checkHit()
+  }, [playerScore])
+
+  useEffect(() => {
+    checkWinner()
+  }, [dealerScore])
+
+  const hit = async () => {
+    try {
+      setHitLoading(true)
+      await getNewCard(playerCards, setPlayerCards, setPlayerScore)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setHitLoading(false)
+    }
+  }
 
   const getNewCard = async (
     cards: Card[],
@@ -66,14 +92,53 @@ const Play = () => {
     return value
   }
 
-  const stay = async () => {
-    if (playerScore > dealerScore && dealerScore < 21) {
-      await getNewCard(dealerCards, setDealerCards, setDealerScore)
+  useEffect(() => {
+    const checkDealerTurn = async () => {
+      if (dealersTurn) {
+        setStayLoading(true)
+        if (
+          dealerScore < 17 &&
+          dealerScore < playerScore &&
+          gameWinnerText === ''
+        ) {
+          setTimeout(async () => {
+            await dealerHit()
+          }, 500)
+        } else {
+          setStayLoading(false)
+          setGameWinnerText(checkWinner())
+          restartGame()
+        }
+      }
     }
-    console.log(checkWinner(playerScore, dealerScore))
+
+    checkDealerTurn()
+  }, [dealerScore, dealersTurn])
+
+  const dealerHit = async () => {
+    try {
+      setStayLoading(true)
+      await getNewCard(dealerCards, setDealerCards, setDealerScore)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  const checkWinner = (playerScore: number, dealerScore: number) => {
+  const stay = async () => {
+    setDealerTurn(true)
+  }
+
+  const checkHit = () => {
+    if (playerScore === 21) {
+      setGameWinnerText('Player wins, blackjack!')
+      restartGame()
+    } else if (playerScore > 21) {
+      setGameWinnerText('Dealer wins, player busted!')
+      restartGame()
+    }
+  }
+
+  const checkWinner = () => {
     if (playerScore > 21) {
       return 'Dealer wins, Player busted!'
     } else if (dealerScore > 21) {
@@ -87,27 +152,109 @@ const Play = () => {
     }
   }
 
-  return (
-    <div>
-      <h1>Play</h1>
-      <div>Deck: {deckId}</div>
-      <p>{playerCards.map((card) => card.value).join(', ')}</p>
-      <p>{dealerCards.map((card) => card.value).join(', ')}</p>
-      <p>Player score: {playerScore}</p>
-      <p>Dealer score: {dealerScore}</p>
+  const restartGame = () => {
+    setTimeout(() => {
+      setPlayerCards([])
+      setDealerCards([])
+      setPlayerScore(0)
+      setDealerScore(0)
+      setGameWinnerText('')
+      setGameRestart((prev) => !prev)
+      setDealerTurn(false)
+    }, 4000)
+  }
 
-      <button
-        className='btn btn-primary'
-        onClick={() => getNewCard(playerCards, setPlayerCards, setPlayerScore)}
+  const RenderScore = (props: { score: number }) => {
+    return (
+      <div className='my-4 grid h-12 w-12 place-items-center rounded-full bg-white font-bold text-black'>
+        {props.score}
+      </div>
+    )
+  }
+
+  const RenderCardsAndScore = (props: {
+    cards: Card[]
+    containerStyle?: string
+  }) => {
+    return (
+      <div
+        className={`relative flex h-full w-screen justify-center ${props.containerStyle}`}
       >
-        Hit
-      </button>
-      <button
-        className='btn btn-primary'
-        onClick={stay}
-      >
-        Stay
-      </button>
+        <RenderCards cards={props.cards} />
+      </div>
+    )
+  }
+
+  const RenderCards = (props: { cards: Card[] }) => {
+    return (
+      <>
+        {props.cards.map((card, index) => {
+          const cardMargin =
+            '[&:nth-child(2)]:ml-[4rem] [&:nth-child(3)]:ml-[8rem] [&:nth-child(4)]:ml-[12rem] [&:nth-child(5)]:ml-[16rem] [&:nth-child(6)]:ml-[20rem] [&:nth-child(7)]:ml-[24rem] [&:nth-child(8)]:ml-[28rem] [&:nth-child(9)]:ml-[32rem] [&:nth-child(10)]:ml-[36rem] [&:nth-child(11)]:ml-[40rem] [&:nth-child(12)]:ml-[44rem]'
+          return (
+            <div
+              className={`absolute flex w-36 flex-col bg-black ${cardMargin}`}
+              key={index}
+            >
+              <img
+                className=''
+                src={cardDictionary[card.code]}
+                alt=''
+              />
+            </div>
+          )
+        })}
+      </>
+    )
+  }
+
+  const RenderGameWinner = () => {
+    return (
+      <div className='text-center'>
+        <p className='text-4xl font-bold'>{gameWinnerText}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className='flex h-screen flex-col'>
+      <section className='flex flex-1 flex-col items-center justify-center'>
+        <RenderScore score={dealerScore} />
+        <RenderCardsAndScore cards={dealerCards} />
+      </section>
+      <section>{gameWinnerText && <RenderGameWinner />}</section>
+      <section className='flex flex-1 flex-col items-center'>
+        <RenderCardsAndScore
+          cards={playerCards}
+          containerStyle='items-end'
+        />
+        <RenderScore score={playerScore} />
+        <section className='flex flex-1 flex-col items-center justify-center'></section>
+        <div className='flex w-full flex-row justify-center gap-12 pb-8'>
+          <button
+            className='btn btn-primary btn-wide'
+            onClick={hit}
+            disabled={!!gameWinnerText}
+          >
+            {hitLoading ? (
+              <span className='loading loading-spinner'></span>
+            ) : (
+              'Hit'
+            )}
+          </button>
+          <button
+            className='btn btn-secondary btn-wide'
+            onClick={stay}
+            disabled={!!gameWinnerText}
+          >
+            {stayLoading ? (
+              <span className='loading loading-spinner'></span>
+            ) : (
+              'Stay'
+            )}
+          </button>
+        </div>
+      </section>
     </div>
   )
 }
