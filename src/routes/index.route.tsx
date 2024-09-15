9b85df9b1de2bfa40db0a96079ac4d2d4c17f1df
Bryan Martinez <bryanmartz972@gmail.com>
2024-09-15 17:42:10 -0600

    feat: 🎸 Implement Home Page design and functionality

--- src/routes/index.route.tsx
+++ src/routes/index.route.tsx
@@ -1,5 +1,40 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Logo } from '@/assets'
+import { Layout, Typography, Container, Button } from '@/components/atoms'
+import { useNavigate } from '@tanstack/react-router'
+
+const Home = () => {
+  const navigate = useNavigate({ from: '/' })
+
+  const navigateToPlay = () => {
+    navigate({ to: '/play' })
+  }
+
+  return (
+    <Layout className='grid place-items-center'>
+      <Container>
+        <Typography
+          variant='h1'
+          className='text-3xl md:text-5xl lg:text-6xl'
+        >
+          Blackjack
+        </Typography>
+      </Container>
+      <img
+        src={Logo}
+        alt='Blackjack Logo'
+        className='h-72 md:h-80 lg:h-96'
+      />
+      <Button
+        type='button'
+        className='nes-btn is-primary !px-12 '
+        text='Play'
+        onClick={navigateToPlay}
+      />
+    </Layout>
+  )
+}
 
 export const Route = createFileRoute('/')({
-  component: () => <div>Hello /!</div>
+  component: Home
 })
